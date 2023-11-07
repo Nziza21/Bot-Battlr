8e71de3ec302b34d40a151443afc8aa44c7ed8b0
Nziza21 <144722415+Nziza21@users.noreply.github.com>
2023-11-07 15:17:04 +0300

    Update App.js

--- botbattlr/src/App.js
+++ botbattlr/src/App.js
@@ -4,15 +4,15 @@ import YourBotArmy from './components/YourBotArmy';
 import './style.css';
 
 function App() {
-const [enlistedBots, setEnlistedBots] = useState([]); 
+  const [enlistedBots, setEnlistedBots] = useState([]); 
 
-return (
-<div className="App">
-<h1>Welcome to Bot Battlr</h1>
-<BotCollection enlistedBots={enlistedBots} setEnlistedBots={setEnlistedBots} />
-<YourBotArmy enlistedBots={enlistedBots} setEnlistedBots={setEnlistedBots} />
-</div>
-);
+  return (
+    <div className="App">
+        <h1>Welcome to Bot Battlr</h1>
+        <BotCollection enlistedBots={enlistedBots} setEnlistedBots={setEnlistedBots} />
+        <YourBotArmy enlistedBots={enlistedBots} setEnlistedBots={setEnlistedBots} />
+    </div>
+  );
 }
 
 export default App;
